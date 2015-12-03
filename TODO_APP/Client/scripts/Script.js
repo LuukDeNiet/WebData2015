95c2cf9a18448f4c3746bcd7378451eadd7c865a
Luuk de Niet <l.f.deniet93@gmail.com>
2015-12-03 20:25:06 +0100

    no message

--- TODO_APP/Client/scripts/Script.js
+++ TODO_APP/Client/scripts/Script.js
@@ -300,10 +300,7 @@ function clone(obj) {
 setInterval(function () {
  //console.log("Fetching the todo list from the server.");
  $.getJSON("/todos", function(data){
- 	if(!isEquivalent(data,tasks)){
- 		console.log(data);
- 		console.log(allTasks());
- 		console.log(data == allTasks());
+ 	if(!data.toString()==tasks.toString()){
  		tasks = data;
  		clearScreen();
  		writeTable();
@@ -311,28 +308,3 @@ setInterval(function () {
  });
  }, 2000);
 
-function isEquivalent(a, b) {
-    // Create arrays of property names
-    var aProps = Object.getOwnPropertyNames(a);
-    var bProps = Object.getOwnPropertyNames(b);
-
-    // If number of properties is different,
-    // objects are not equivalent
-    if (aProps.length != bProps.length) {
-        return false;
-    }
-
-    for (var i = 0; i < aProps.length; i++) {
-        var propName = aProps[i];
-
-        // If values of same property are not equal,
-        // objects are not equivalent
-        if (a[propName] !== b[propName]) {
-            return false;
-        }
-    }
-
-    // If we made it this far, objects
-    // are considered equivalent
-    return true;
-}
